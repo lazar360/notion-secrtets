48ced091ccfa6438c7f67762d977c35114fd2f3c
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-09-18 11:02:34 +0200

    initial commit

---
+++ src/main/java/dev/ngautier/notionsecrets/NotionSecretsApplication.java
@@ -0,0 +1,13 @@
+package dev.ngautier.notionsecrets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionSecretsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionSecretsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ngautier/notionsecrets/NotionSecretsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ngautier.notionsecrets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionSecretsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
